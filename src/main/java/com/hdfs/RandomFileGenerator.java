693f8e42391c83d4833913edfcb2d74ab9613795
Azman Singh Garcha <asg8bz@virginia.edu>
2019-04-15 13:16:23 -0400

    added support for multiple clusters to randomFileGenerator

--- src/main/java/com/hdfs/RandomFileGenerator.java
+++ src/main/java/com/hdfs/RandomFileGenerator.java
@@ -36,31 +36,32 @@ public class RandomFileGenerator {
             }
         }
     }
-    
+
     public String generateRandomString(int length) {
-    	  
+
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = length;
         Random random = new Random();
         StringBuilder buffer = new StringBuilder(targetStringLength);
         for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int) 
+            int randomLimitedInt = leftLimit + (int)
               (random.nextFloat() * (rightLimit - leftLimit + 1));
             buffer.append((char) randomLimitedInt);
         }
         String generatedString = buffer.toString();
-	    
+
         return generatedString;
     }
-    
+
     public static void main(String[] args) throws IOException {
+
         Random ran = new Random();
-        
-        //Scanner reader = new Scanner(System.in);  // Reading from System.in
-        //System.out.println("Enter cluster number: ");
-        //int n = reader.nextInt();
-        for(int n = 1; n <= 3; n++) {
+
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter number of clusters: ");
+        int c_number = reader.nextInt();
+        for(int n = 1; n <= c_number; n++) {
         	for(int i = 0;i <= 999;i++){
         		//int line = ran.nextInt(10000000-1000000+1) + 1000000;
         		int index = 1000*n + i;
